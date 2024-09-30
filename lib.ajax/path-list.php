962b6d9aec7dfd06da92bf0ef923d12d34f6f201
Kamshory, MT <kamshory@alto.id>
2024-09-30 09:10:10 +0700

    Update

--- lib.ajax/path-list.php
+++ lib.ajax/path-list.php
@@ -1,17 +1,9 @@
 
 <?php
 
+use MagicObject\Response\PicoResponse;
+
 require_once dirname(__DIR__) . "/inc.app/auth.php";
 
-$constSelected = ' selected';
-
 $moduleLocation = $appConfig->getApplication() != null ? $appConfig->getApplication()->getBaseModuleDirectory() : array();
-if (!empty($moduleLocation)) 
-{
-    foreach ($moduleLocation as $key => $value) 
-    {
-    ?>
-    <option value="<?php echo $value->getPath(); ?>"<?php echo $value->getActive() ? " selected":"";?>><?php echo $value->getName(); ?> - <?php echo $value->getPath(); ?></option>
-    <?php
-    }
-}
+PicoResponse::sendJSON($moduleLocation);
