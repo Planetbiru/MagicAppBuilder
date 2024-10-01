4fb31b8b7b5824171fd12cc35a0aae0e45770108
Kamshory, MT <kamshory@yahoo.com>
2024-10-02 02:08:55 +0700

    Update ResponseUtil.php

--- inc.lib/classes/AppBuilder/Util/ResponseUtil.php
+++ inc.lib/classes/AppBuilder/Util/ResponseUtil.php
@@ -11,7 +11,7 @@ class ResponseUtil
      * @param boolean $prettify
      * @return void
      */
-    public static function sendJSON($data, $prettify = false)
+    public static function sendJSON($data, $prettify = false, $async = false)
     {
         $body = null;
         if ($data != null) {
@@ -26,6 +26,27 @@ class ResponseUtil
             }
         }
         header("Content-type: application/json");
-        echo $body;
+        if ($async) {
+            if (function_exists('ignore_user_abort')) {
+                ignore_user_abort(true);
+            }
+            ob_start();
+
+            if ($body != null) {
+                echo $body;
+            }
+        }
+        header("Connection: close");
+
+        if ($async) {
+            ob_end_flush();
+            ob_flush();
+            flush();
+            if (function_exists('fastcgi_finish_request')) {
+                fastcgi_finish_request();
+            }
+        } else if ($body != null) {
+            echo $body;
+        }
     }
 }
