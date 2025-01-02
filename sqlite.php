5934ffc2493a15050c61c505b2893a4d63fcd5e9
Kamshory, MT <kamshory@yahoo.com>
2025-01-02 08:11:05 +0700

    Update sqlite.php

--- sqlite.php
+++ sqlite.php
@@ -11,14 +11,18 @@
         <div class="message-box">
             <h1>SQLite is Not Available</h1>
             <p>It seems that SQLite is not available on your system. Please make sure that SQLite is properly installed and enabled.</p>
-            <p>If you need help setting up SQLite, please refer to the installation documentation or contact your administrator.</p>
             <?php
             // Mendapatkan semua ekstensi yang aktif di PHP
             $extensions = get_loaded_extensions();
             if(!in_array('pdo_sqlite', $extensions))
             {
                 ?>
-                <p><strong>pdo_sqlite extension is not loaded</strong></p>
+                <p>To resolve this issue, verify that the <strong>pdo_sqlite</strong> extension is loaded. If it is not, you may need to install or enable it.</p>
+                <p>For assistance with setting up SQLite, please refer to the installation documentation or contact your system administrator.</p>
+                <p>Follow links: 
+                    <br>
+                    <a href="https://www.php.net/manual/en/sqlite3.setup.php" target="_blank">https://www.php.net/manual/en/sqlite3.setup.php</a>
+                </p>
                 <?php
             }
             ?>
