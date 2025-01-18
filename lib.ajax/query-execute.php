d96e15408bc131848f3a503031568f60fbc52a7f
Kamshory, MT <kamshory@yahoo.com>
2025-01-18 23:42:23 +0700

    Update

--- lib.ajax/query-execute.php
+++ lib.ajax/query-execute.php
@@ -8,15 +8,18 @@ require_once dirname(__DIR__) . "/inc.app/auth.php";
 require_once dirname(__DIR__) . "/inc.app/database.php";
 
 $inputPost = new InputPost();
-$queries = PicoDatabaseUtil::splitSql($inputPost->getQuery());
-foreach($queries as $query)
+if($database->isConnected())
 {
-    try
+    $queries = PicoDatabaseUtil::splitSql($inputPost->getQuery());
+    foreach($queries as $query)
     {
-        $database->execute($query['query']);
-    }
-    catch(Exception $e)
-    {
-        error_log($e->getMessage());
+        try
+        {
+            $database->execute($query['query']);
+        }
+        catch(Exception $e)
+        {
+            error_log($e->getMessage());
+        }
     }
 }
