34b435205f4f0376af3f9f00f536f2dcf4ff38bf
Kamshory, MT <kamshory@alto.id>
2025-04-01 07:02:41 +0700

    Update File Manager and Fix jQuery

--- filemanager/index.php
+++ filemanager/index.php
@@ -1,33 +1,35 @@
 <?php
-require_once __DIR__ . "/func.php";
 ?><!DOCTYPE html>
 <html lang="id">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>File Manager Sederhana</title>
+    <title>File Manager</title>
     <link rel="stylesheet" href="assets/style.css">
 </head>
 <body>
 
 <div id="container">
     <div id="left-panel">
-        <h3>Direktori</h3>
-        <ul id="dir-tree">
-            <?php
-            // Tampilkan tree direktori mulai dari folder root
-            echo getDirTree('./');
-            ?>
+        <h3>Directory</h3>
+        <ul id="dir-tree" data-base-directory="./">
         </ul>
     </div>
 
     <div id="right-panel">
-        <h3>Isi File</h3>
-        <div id="file-content"></div>
+        <h3>File Contents</h3>
+        <div id="file-content">
+            <div class="media-display image-mode"></div>
+            <div class="code text-mode">
+                <textarea id="code" name="code"></textarea>
+            </div>
+        </div>
     </div>
 </div>
-
-<script src="../lib.assets/jquery/js/jquery-1.10.2.js"></script>
+<link rel="stylesheet" href="../lib.assets/cm/lib/codemirror.css">
+<script src="../lib.assets/cm/lib/codemirror.js"></script>
+<script src="../lib.assets/cm/addon/mode/loadmode.js"></script>
+<script src="../lib.assets/cm/mode/meta.js"></script>
 <script src="assets/script.js"></script>
 
 </body>
