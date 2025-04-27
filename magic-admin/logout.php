6bf2649e3b260e032d8ffc7ac7a8e030414d2e92
Kamshory, MT <kamshory@yahoo.com>
2025-04-28 06:30:59 +0700

    Update session

--- magic-admin/logout.php
+++ magic-admin/logout.php
@@ -2,5 +2,8 @@
 
 require_once dirname(__DIR__) . "/inc.app/app.php";
 require_once dirname(__DIR__) . "/inc.app/sessions.php";
-$sessions->destroy();
+
+unset($sessions->magicUsername);
+unset($sessions->magicUserPassword);
+
 header("Location: ./");
