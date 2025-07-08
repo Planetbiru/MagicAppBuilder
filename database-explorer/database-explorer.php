dedf1098012244cbad5805bcad5773336eaf5be2
Kamshory, MT <kamshory@alto.id>
2025-07-08 11:15:27 +0700

    Export entity structure and data from Entity Editor to Database Explorer

--- database-explorer/database-explorer.php
+++ database-explorer/database-explorer.php
@@ -32,6 +32,9 @@ if(!isset($databaseName))
     <script src="../lib.assets/datetimepicker/jquery.datetimepicker.full.min.js"></script>
     <script src="../lib.assets/js/TableParser.min.js"></script>
     <script src="../lib.assets/js/SQLConverter.min.js"></script>
+    <script src="../lib.assets/js/Column.min.js"></script>
+    <script src="../lib.assets/js/Entity.min.js"></script>
+    <script src="../lib.assets/js/Diagram.min.js"></script>
     <script src="../lib.assets/js/EntityEditor.min.js"></script>
     <script src="../lib.assets/js/EntityRenderer.min.js"></script>
     <script src="../lib.assets/js/ResizablePanel.min.js"></script>
