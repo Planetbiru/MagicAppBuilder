abfa75ae1f52deba581f3cc8d486a9b23aef32c0
Kamshory, MT <kamshory@alto.id>
2025-07-28 16:44:01 +0700

    Remove Sort by Dependency Depth button

--- lib.ajax/database-table-list.php
+++ lib.ajax/database-table-list.php
@@ -1,29 +1,3 @@
 <?php
 
-use AppBuilder\AppDatabase;
-use AppBuilder\Util\ResponseUtil;
-
-require_once dirname(__DIR__) . "/inc.app/auth.php";
-require_once dirname(__DIR__) . "/inc.app/database.php";
-
-if(!$database->isConnected())
-{
-    ResponseUtil::sendJSON(new stdClass);
-    exit();
-}
-
-try {
-    $databaseType = $database->getDatabaseType();
-    $schemaName = $databaseConfig->getDatabaseSchema();
-    $databaseName = $databaseConfig->getDatabaseName();
-
-    $tables = AppDatabase::getTableList($database, $databaseName, $schemaName);
-
-    // Send the JSON response
-    ResponseUtil::sendJSON($tables);
-
-} catch (Exception $e) {
-    // Log the error for debugging purposes
-    error_log("Error: " . $e->getMessage());
-    ResponseUtil::sendJSON(["error" => "An error occurred while processing your request."]);
-}
+require_once __DIR__ . "/database-sort-by-dependency-depth.php";
